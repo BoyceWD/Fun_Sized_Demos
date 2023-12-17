a31e88376a8995f71b3d67901bbde0d78b99923e
Boyce Davis <boyce.w.davis@gmail.com>
2023-12-17 10:22:12 -0600

    reworked loop inside function sliceOffLastChar

--- Base2ToBase10/tests/sliceOffLastChar.js
+++ Base2ToBase10/tests/sliceOffLastChar.js
@@ -1,15 +1,21 @@
 function sliceOffLastChar(str){
-    s = "";
-    sliceEndPos = str.length;
-    for(i = 0; i < str.length; i++) {
-        
-        positionOfSecondToLastElement = str.length - 1;
-        let lastBit = str.slice(sliceEndPos-1);
+    var s = "";
+    var sliceEndPos = str.length;
+    var sliceStartPos = sliceEndPos -1;
+    //for(i = 0; i < str.length; i++) {
+    while(sliceEndPos > 0){
+        sliceStartPos = sliceEndPos -1;
+        //positionOfSecondToLastElement = str.length - 1;
+        //var lastBit = str.slice(sliceEndPos-1);
+        var lastBit = str.slice(sliceStartPos, sliceEndPos);
         sliceEndPos -= 1;
         s += lastBit;
+        lastBit = "";
     }
     return s;
     
 }
 // 5:12PM 12.7.2023: Why does this function return '1110110011', when given an input of '0011'?
+// 4:51PM 12.16.2023: This function can retrieve a strings characters last to first and create a reversed string of the input
+// but for some resone does not return the reversed value when run in a unit test yet
 module.exports = sliceOffLastChar;
